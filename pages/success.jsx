734c5428908376220991235b9944337827eacca9
CodeBrewLatte <steve@rocketoptionu.com>
2023-01-01 14:42:36 -0500

    fix 4

--- pages/success.jsx
+++ pages/success.jsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { useState, useEffect } from 'react'
-import SignIn from '/components/SignIn'
+import SignIn from '../components/SignIn'
 
 export default function Home() {
   useEffect(() => setSignUp(true),[])
